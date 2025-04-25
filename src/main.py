57b4caa5ac068e8cc38eaf0303db68d8e0196166
Suraj S <surajsuri279@gmail.com>
2025-04-25 16:17:36 +0530

    done

--- src/main.py
+++ src/main.py
@@ -12,37 +12,8 @@ st.set_page_config(layout="wide")
 st.title("Credit Risk Prediction App")
 
 # Sidebar for data upload
-st.sidebar.header("Upload Dataset")
-uploaded_file = st.sidebar.file_uploader("Upload German Credit Data CSV", type=["csv"])
-
 # Load and preprocess data
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-    df.rename(columns={
-        'A11': 'Status of existing checking account',
-        '6': 'Duration in month',
-        'A34': 'Credit history',
-        'A43': 'Purpose',
-        '1169': 'Credit amount',
-        'A65': 'Savings account',
-        'A75': 'Present employment since',
-        '4': 'Installment rate in percentage of disposable income',
-        'A93': 'Personal status and sex',
-        'A101': 'Other debtors',
-        '4.1': 'Present residence since',
-        'A121': 'Property',
-        '67': 'Age in years',
-        'A143': 'Other installment plans',
-        'A152': 'Housing',
-        '2': 'Number of existing credits at this bank',
-        'A173': 'Job',
-        '1': 'Number of dependents',
-        'A192': 'Telephone',
-        'A201': 'Foreign worker',
-        '1.1': 'Risk'
-    }, inplace=True)
-else:
-    df = load_data()
+df = load_data()
 
 st.subheader("Dataset Preview")
 st.dataframe(df.head())
